2965e164dfd27b2bce930bf65180d78183616a97
_Ankush_sharma <107449662+AnkushSharma01@users.noreply.github.com>
2024-09-23 01:17:59 +0530

    Update MessageContainer.jsx

--- frontend/src/home/MessageContainer.jsx
+++ frontend/src/home/MessageContainer.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react'
-import userConversation from '../Zustans/userConversation';
+import userConversation from '../Zustans/userConversation.jsx';
 import { useAuth } from '../context/AuthContext';
 import { TiMessages } from 'react-icons/ti';
 import { IoArrowBackSharp, IoSend } from 'react-icons/io5';
