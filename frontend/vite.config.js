bbcabe92c8fdbe2ba0fa7703385ff2a5cb0e4572
_Ankush_sharma <107449662+AnkushSharma01@users.noreply.github.com>
2024-10-25 14:46:12 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target: 'http://localhost:3000',
+        target: 'https://chatapp-mern-83ve.onrender.com',
         secure:false
       }
     }
